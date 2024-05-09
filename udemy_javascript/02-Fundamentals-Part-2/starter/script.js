fda6c388ad97d6e523f8e856c4ded2fb0f856d61
Eui Lee <lechan1317@gmail.com>
2024-05-09 13:38:46 +0900

    Feat: test

---
+++ udemy_javascript/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const euiclee = {
+	firstName: 'euiclee',
+	lastName: 'lee',
+	age: 2037 - 2002,
+	job: 'studtent',
+  friends: ['Michael', 'Peter', 'Steven'],
+};
+console.log(euiclee);

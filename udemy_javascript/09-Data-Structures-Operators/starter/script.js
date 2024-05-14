c8025f1acca03c6bb82a8e114d7009fa06466f4c
Eui Lee <lechan1317@gmail.com>
2024-05-14 18:03:31 +0900

    Feat: test

--- udemy_javascript/09-Data-Structures-Operators/starter/script.js
+++ udemy_javascript/09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -22,8 +26,519 @@ const restaurant = {
       close: 23,
     },
     sat: {
-      open: 0, // Open 24 hours
+      open: 0,
       close: 24,
     },
   },
 };
+
+console.log('a+very+nice+string'.split('+'));
+console.log('Jonas Schmedtmann'.split(' '));
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(25, '+').padEnd(35, '+'));
+console.log('Jonas'.padStart(25, '+').padEnd(35, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+};
+
+// const airline = 'TAP Air Portugal';
+
+// console.log(airline.toLowerCase());
+// console.log(airline.toUpperCase());
+
+// // Fix capitalization in name
+// const passenger = 'jOnAS';
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// console.log(passengerCorrect);
+
+// const email = 'hello@jonas.io';
+// const loginEmail = ' Hello@Jonas.Io \n';
+
+// const nomalizedEmail = loginEmail.toLowerCase().trim();
+
+// console.log(nomalizedEmail);
+// console.log(email === nomalizedEmail);
+
+// const plane = 'A320';
+
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log('B737'[0]);
+
+// console.log(airline.length);
+// console.log('B737'.length);
+
+// console.log(airline.indexOf('r'));
+// console.log(airline.lastIndexOf('r'));
+// console.log(airline.indexOf('Portugal'));
+
+// console.log(airline.slice(4));
+// console.log(airline.slice(4, 7));
+
+// console.log(airline.slice(0, airline.indexOf(' ')));
+// console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+// console.log(airline.slice(-2));
+
+// const checkMiddleSeat = function (seat) {
+//   const s = seat.slice(-1);
+//   if (s === 'B' || s === 'E') {
+//     console.log('You got the middle seat');
+//   } else {
+//     console.log('You got lucky');
+//   }
+// }
+
+// checkMiddleSeat('11B');
+
+// const question = new Map([
+//   ['question', 'What is the best programming language?'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'JavaScript'],
+//   ['correct', 3],
+//   [true, 'Correct'],
+//   [false, 'Try again'],
+// ]);
+// console.log(question);
+
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+
+// console.log(hoursMap);
+
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// // }
+
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// rest.set(2, 'Italian');
+// console.log(rest);
+
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open')
+//   .set(false, 'We are closed');
+
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// console.log(rest);
+// console.log(rest.size);
+// rest.clear();
+
+// const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza']); console.log(ordersSet);
+
+// console.log(new Set('Jonas'));
+// console.log(ordersSet.size);
+// console.log(ordersSet.has('Pizza'));
+// console.log(ordersSet.has('Bread'));
+// ordersSet.add('Garlic Bread');
+// ordersSet.delete('Risotto');
+// console.log(ordersSet);
+
+// for (const order of ordersSet) console.log(order);
+
+// const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+
+// for (const day of Object.keys(openingHours)) {
+//   console.log(day);
+// }
+
+// for (const [key, {open, close}] of Object.entries(openingHours)) {
+//   console.log(`On ${key} we open at ${open} and close at ${close}`);
+// }
+
+// if (restaurant.openingHours && restaurant.openingHours.mon) {
+//   console.log(restaurant.openingHours.mon.open);
+// }
+
+// console.log(restaurant.openingHours?.mon?.open);
+
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const item of menu) {
+//   console.log(item);
+// }
+
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
+
+// const game = {
+//   temp1: 'Bayern Munich',
+//   temp2: 'Borrussia Dortmund',
+//   player: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// const [players1, players2] = game.player;
+// console.log(players1, players2);
+
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
+
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
+
+// const {odds: {team1, x: draw, team2}} = game;
+// console.log(team1, draw, team2);
+
+// const printGoals = function (...players) {
+//   console.log(`${players.length} goals were scored`);
+// }
+// printGoals(...game.scored);
+
+// team1 < team2 && console.log('Team 1 is more likely to win');
+// team1 > team2 && console.log('Team 2 is more likely to win');
+
+// rest1.numGuests ??= 10;
+
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
+
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
+
+// console.log('Jonas' || 3);
+
+// // && and || are short-circuiting
+// restaurant.starterMenu && console.log(restaurant.starterMenu);
+
+// // SPREAD, because on RIGHT side of =
+// const arr = [1, 2, ...[3, 4]];
+
+// // REST, because on LEFT side of =
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
+
+// const [pizza, , risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherFood);
+
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+// };
+
+// add(2, 3);
+// add(5, 7, 9, 11);
+// add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+// const {name, openingHours, categories} = restaurant;
+// console.log(name, openingHours, categories);
+
+// const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+// console.log(restaurantName, hours, tags);
+
+// const {menu = [], starterMenu: starters = []} = restaurant;
+// console.log(menu, starters);
+
+// let a = 111;
+// let b = 999;
+// const obj = {a: 23, b: 7, c: 14};
+// ({a, b} = obj);
+// console.log(a, b);
+
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+
+// const newArr = [1, 2, ...arr];
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// // Copy array
+// const mainMenuCopy = [...restaurant.mainMenu];
+
+// // Join 2 arrays
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+
+// const arr = [7, 8, 9];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// // const temp = main;
+// // main = secondary;
+// // secondary = temp;
+// // console.log(main, secondary);
+
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
+
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// const nested = [2, 4, [5, 6]];
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// const books = [
+//   {
+//     title: 'Algorithms',
+//     author: ['Robert Sedgewick', 'Kevin Wayne'],
+//     publisher: 'Addison-Wesley Professional',
+//     publicationDate: '2011-03-24',
+//     edition: 4,
+//     keywords: ['computer science', 'programming', 'algorithms', 'data structures', 'java', 'math', 'software', 'engineering'],
+//     pages: 976,
+//     format: 'hardcover',
+//     ISBN: '9780321573513',
+//     language: 'English',
+//     programmingLanguage: 'Java',
+//     onlineContent: true,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.41,
+//         ratingsCount: 1733,
+//         reviewsCount: 63,
+//         fiveStarRatingCount: 976,
+//         oneStarRatingCount: 13
+//       }
+//     },
+//     highlighted: true
+//   },
+//   {
+//     title: 'Structure and Interpretation of Computer Programs',
+//     author: ['Harold Abelson', 'Gerald Jay Sussman', 'Julie Sussman (Contributor)'],
+//     publisher: 'The MIT Press',
+//     publicationDate: '2022-04-12',
+//     edition: 2,
+//     keywords: ['computer science', 'programming', 'javascript', 'software', 'engineering'],
+//     pages: 640,
+//     format: 'paperback',
+//     ISBN: '9780262543231',
+//     language: 'English',
+//     programmingLanguage: 'JavaScript',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.36,
+//         ratingsCount: 14,
+//         reviewsCount: 3,
+//         fiveStarRatingCount: 8,
+//         oneStarRatingCount: 0
+//       }
+//     },
+//     highlighted: true
+//   },
+//   {
+//     title: 'Computer Systems: A Programmer\'s Perspective',
+//     author: ['Randal E. Bryant', 'David Richard O\'Hallaron'],
+//     publisher: 'Prentice Hall',
+//     publicationDate: '2002-01-01',
+//     edition: 1,
+//     keywords: ['computer science', 'computer systems', 'programming', 'software', 'C', 'engineering'],
+//     pages: 978,
+//     format: 'hardcover',
+//     ISBN: '9780130340740',
+//     language: 'English',
+//     programmingLanguage: 'C',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.44,
+//         ratingsCount: 1010,
+//         reviewsCount: 57,
+//         fiveStarRatingCount: 638,
+//         oneStarRatingCount: 16
+//       }
+//     },
+//     highlighted: true
+//   },
+//   {
+//     title: 'Operating System Concepts',
+//     author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
+//     publisher: 'John Wiley & Sons',
+//     publicationDate: '2004-12-14',
+//     edition: 10,
+//     keywords: ['computer science', 'operating systems', 'programming', 'software', 'C', 'Java', 'engineering'],
+//     pages: 921,
+//     format: 'hardcover',
+//     ISBN: '9780471694663',
+//     language: 'English',
+//     programmingLanguage: 'C, Java',
+//     onlineContent: false,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 3.9,
+//         ratingsCount: 2131,
+//         reviewsCount: 114,
+//         fiveStarRatingCount: 728,
+//         oneStarRatingCount: 65
+//       }
+//     }
+//   },
+//   {
+//     title: 'Engineering Mathematics',
+//     author: ['K.A. Stroud', 'Dexter J. Booth'],
+//     publisher: 'Palgrave',
+//     publicationDate: '2007-01-01',
+//     edition: 14,
+//     keywords: ['mathematics', 'engineering'],
+//     pages: 1288,
+//     format: 'paperback',
+//     ISBN: '9781403942463',
+//     language: 'English',
+//     programmingLanguage: null,
+//     onlineContent: true,
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.35,
+//         ratingsCount: 370,
+//         reviewsCount: 18,
+//         fiveStarRatingCount: 211,
+//         oneStarRatingCount: 6
+//       }
+//     },
+//     highlighted: true
+//   },
+//   {
+//     title: 'The Personal MBA: Master the Art of Business',
+//     author: 'Josh Kaufman',
+//     publisher: 'Portfolio',
+//     publicationDate: '2010-12-30',
+//     keywords: ['business'],
+//     pages: 416,
+//     format: 'hardcover',
+//     ISBN: '9781591843528',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.11,
+//         ratingsCount: 40119,
+//         reviewsCount: 1351,
+//         fiveStarRatingCount: 18033,
+//         oneStarRatingCount: 1090
+//       }
+//     }
+//   },
+//   {
+//     title: 'Crafting Interpreters',
+//     author: 'Robert Nystrom',
+//     publisher: 'Genever Benning',
+//     publicationDate: '2021-07-28',
+//     keywords: ['computer science', 'compilers', 'engineering', 'interpreters', 'software', 'engineering'],
+//     pages: 865,
+//     format: 'paperback',
+//     ISBN: '9780990582939',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.7,
+//         ratingsCount: 253,
+//         reviewsCount: 23,
+//         fiveStarRatingCount: 193,
+//         oneStarRatingCount: 0
+//       }
+//     }
+//   },
+//   {
+//     title: 'Deep Work: Rules for Focused Success in a Distracted World',
+//     author: 'Cal Newport',
+//     publisher: 'Grand Central Publishing',
+//     publicationDate: '2016-01-05',
+//     edition: 1,
+//     keywords: ['work', 'focus', 'personal development', 'business'],
+//     pages: 296,
+//     format: 'hardcover',
+//     ISBN: '9781455586691',
+//     language: 'English',
+//     thirdParty: {
+//       goodreads: {
+//         rating: 4.19,
+//         ratingsCount: 144584,
+//         reviewsCount: 11598,
+//         fiveStarRatingCount: 63405,
+//         oneStarRatingCount: 1808
+//       }
+//     },
+//     highlighted: true
+//   }
+// ];
+
+// const [firstBook, secondBook] = books;
+// console.log(firstBook, secondBook);
+
+// const [, , thirdBook] = books;
+// console.log(thirdBook);
+
+// const ratings = [['rating', 4.19], ['ratingsCount', 144584]];
+
+// const [[, rating], [, ratingsCount]] = ratings;
+
+// console.log(rating, ratingsCount);
+
+// const ratingStars = [63405, 1808];
+
+// const [fiveStarRatingCount, oneStarRatingCount] = ratingStars;
+
+// console.log(fiveStarRatingCount, oneStarRatingCount);

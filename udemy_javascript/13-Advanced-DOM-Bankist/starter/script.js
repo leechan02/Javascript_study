c6033a9f720667e23818455279f41a9db22ea218
Eui Lee <lechan1317@gmail.com>
2024-07-02 17:16:27 +0900

    feat: observer api

--- udemy_javascript/13-Advanced-DOM-Bankist/starter/script.js
+++ udemy_javascript/13-Advanced-DOM-Bankist/starter/script.js
@@ -136,10 +136,42 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
 // Sticky navigation
-window.addEventListener('scroll', function () {
-  if (window.scrollY > nav.getBoundingClientRect().height) nav.classList.add('sticky');
+// window.addEventListener('scroll', function () {
+//   if (window.scrollY > nav.getBoundingClientRect().height) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// });
+
+// Sticky navigation: Intersection Observer API
+
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+headerObserver.observe(header);
 
 ///////////////////////////////////////
 ///////////////////////////////////////

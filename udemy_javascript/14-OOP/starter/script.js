7f8cbdc7e85430125d5b667ac169cacdfd9a66aa
Eui Lee <lechan1317@gmail.com>
2024-07-03 20:39:50 +0900

    feat: object create

--- udemy_javascript/14-OOP/starter/script.js
+++ udemy_javascript/14-OOP/starter/script.js
@@ -299,7 +299,6 @@ tesla.chargeBattery(90);
 console.log(tesla);
 tesla.brake();
 tesla.accelerate();
-*/
 
 // Inheritance between "classes": ES6 Classes
 
@@ -353,3 +352,31 @@ class StudentCL extends PersonCl {
 const martha = new StudentCL('Martha Jones', 2012, 'Computer Science');
 martha.introduce();
 martha.calcAge();
+*/
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function(firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function() {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();

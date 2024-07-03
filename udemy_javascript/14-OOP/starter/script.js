daebc4d8eed6180d81b4a2473dacf6b1f57dd954
Eui Lee <lechan1317@gmail.com>
2024-07-03 15:15:39 +0900

    feat: prototype

--- udemy_javascript/14-OOP/starter/script.js
+++ udemy_javascript/14-OOP/starter/script.js
@@ -24,3 +24,27 @@ const euiclee = new Person('euiclee', 2002);
 console.log(matilda, euiclee);
 
 console.log(jonas instanceof Person); // true
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function() {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species = 'Homo Sapioens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));

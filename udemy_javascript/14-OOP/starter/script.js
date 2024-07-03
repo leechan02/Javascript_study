82d03117a95ed592efc8b9eea47d21b6ae517083
Eui Lee <lechan1317@gmail.com>
2024-07-03 20:59:59 +0900

    feat: private

--- udemy_javascript/14-OOP/starter/script.js
+++ udemy_javascript/14-OOP/starter/script.js
@@ -386,15 +386,20 @@ class Account {
     this.owner = owner;
     this.currency = currency;
     this.pin = pin;
-    this.movements = [];
+    // Protected properties
+    this._movements = [];
     this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
   // Public interface
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {

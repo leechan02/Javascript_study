33bd8e7c5799dc3b7a001966a5e3dc10b11a0def
Eui Lee <lechan1317@gmail.com>
2024-07-03 15:07:41 +0900

    constructor functions

--- udemy_javascript/14-OOP/starter/script.js
+++ udemy_javascript/14-OOP/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const Person = function(firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  // this.calcAge = function() {
+  //   console.log(2037 - this.birthYear);
+  // };
+}
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const euiclee = new Person('euiclee', 2002);
+console.log(matilda, euiclee);
+
+console.log(jonas instanceof Person); // true

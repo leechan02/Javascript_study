c7295c3dc863bc7c8d5c855c3111f47a37a94ead
Eui Lee <lechan1317@gmail.com>
2024-07-11 13:59:07 +0900

    feat: comment

--- udemy_javascript/16-Asynchronous/starter/script.js
+++ udemy_javascript/16-Asynchronous/starter/script.js
@@ -112,8 +112,8 @@ setTimeout(() => {
 //     });
 // };
 
-const request = fetch('https://restcountries.com/v3.1/name/korea');
-console.log(request);
+// const request = fetch('https://restcountries.com/v3.1/name/korea');
+// console.log(request);
 
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText('beforeend', msg);
@@ -183,38 +183,39 @@ btn.addEventListener('click', function () {
 
 // console.log('Test end');
 
-const lotteryPromise = new Promise(function (resolve, reject) {
-  console.log('Lottery draw is happening 🔮');
-  setTimeout(function () {
-    if (Math.random() >= 0.5) {
-      resolve('You WIN 💰');
-    } else {
-      reject(new Error('You lost your money 💩'));
-    }
-  }, 2000);
-});
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log('Lottery draw is happening 🔮');
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       resolve('You WIN 💰');
+//     } else {
+//       reject(new Error('You lost your money 💩'));
+//     }
+//   }, 2000);
+// });
 
-lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+// lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
 
-// Promisifying setTimeout
-const wait = function (seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000);
-  });
-};
+// // Promisifying setTimeout
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
 
-wait(1).then(() => {
-  console.log('1 second passed');
-  return wait(1);
-}).then(() => {
-  console.log('2 seconds passed');
-  return wait(1);
-}).then(() => {
-  console.log('3 seconds passed');
-  return wait(1);
-}).then(() => {
-  console.log('4 seconds passed');
-});
+// wait(1).then(() => {
+//   console.log('1 second passed');
+//   return wait(1);
+// }).then(() => {
+//   console.log('2 seconds passed');
+//   return wait(1);
+// }).then(() => {
+//   console.log('3 seconds passed');
+//   return wait(1);
+// }).then(() => {
+//   console.log('4 seconds passed');
+// });
 
-Promise.resolve('abc').then(x => console.log(x));
-Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+// Promise.resolve('abc').then(x => console.log(x));
+// Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+

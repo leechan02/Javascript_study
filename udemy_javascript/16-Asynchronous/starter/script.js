25718c8329a9f2d0b88901b071d35fbd764e7bdc
Eui Lee <lechan1317@gmail.com>
2024-07-11 13:41:39 +0900

    feat: event loop

--- udemy_javascript/16-Asynchronous/starter/script.js
+++ udemy_javascript/16-Asynchronous/starter/script.js
@@ -172,3 +172,14 @@ btn.addEventListener('click', function () {
 // }
 
 // whereAmI(52.508, 13.381);
+
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end');

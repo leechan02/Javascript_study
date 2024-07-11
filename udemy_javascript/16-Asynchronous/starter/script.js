f66d06af55a3a5cdd89dfd5cfc2df0339f79c731
Eui Lee <lechan1317@gmail.com>
2024-07-11 14:58:05 +0900

    feat: coding 2

--- udemy_javascript/16-Asynchronous/starter/script.js
+++ udemy_javascript/16-Asynchronous/starter/script.js
@@ -248,3 +248,24 @@ setTimeout(() => {
 //     .catch(err => console.error(`${err.message}`));
 // }
 
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+createImage('img/img-1.jpg').then(img => {
+  console.log('Image loaded');
+}).catch(err => console.error(err));

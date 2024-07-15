5084c3435907edfb59370eb35c8b8c0b8b4f8c6d
Eui Lee <lechan1317@gmail.com>
2024-07-15 11:23:52 +0900

    feat: module comment

--- udemy_javascript/17-Modern-JS-Modules-Tooling/starter/script.js
+++ udemy_javascript/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -2,15 +2,38 @@
 // addToCart('apple', 4);
 // console.log(price, tq);
 
-console.log('Importing module');
+// console.log('Importing module');
 
 // import * as ShoppingCart from './shoppingCart.js';
 // ShoppingCart.addToCart('apple', 4);
 // console.log(ShoppingCart.totalPrice);
 
-import add, { cart } from './shoppingCart.js';
-add('apple', 5);
-add('bread', 5);
-add('apples', 5);
+// import add, { cart } from './shoppingCart.js';
+// add('apple', 5);
+// add('bread', 5);
+// add('apples', 5);
 
-console.log(cart);
+// console.log(cart);
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+// const getLastPost = async function() {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await res.json();
+//   console.log(data);
+
+//   return {title: data.at(-1).title, body: data.at(-1).body};
+// }
+
+// const lastPost = getLastPost();
+// console.log(lastPost);
+
+// lastPost.then(data => console.log(data));
+
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+

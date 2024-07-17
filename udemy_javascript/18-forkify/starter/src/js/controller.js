5142d7c96845f7dbe2da4b104535ecb444695c36
Eui Lee <lechan1317@gmail.com>
2024-07-17 20:42:42 +0900

    feat: localstorage bookmark

--- udemy_javascript/18-forkify/starter/src/js/controller.js
+++ udemy_javascript/18-forkify/starter/src/js/controller.js
@@ -22,13 +22,16 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultPage());
+    
+    // 1) Update bookmarks view
     bookmarksView.update(model.state.bookmarks);
 
-    // 1) Loading recipe
+    // 2) Loading recipe
     await model.loadRecipe(id);
-
-    // 2) Rendering recipe
+    
+    // 3) Rendering recipe
     recipeView.render(model.state.recipe);
+
   } catch (err) {
     recipeView.renderError();
   }
@@ -84,7 +87,12 @@ const controlAddBookmark = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+}
+
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);

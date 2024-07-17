fed9b466848f0d40a4fb35479b90c6aa924f56cb
Eui Lee <lechan1317@gmail.com>
2024-07-17 15:35:19 +0900

    feat: page

--- udemy_javascript/18-forkify/starter/src/js/controller.js
+++ udemy_javascript/18-forkify/starter/src/js/controller.js
@@ -40,7 +40,7 @@ const controlSearchResults = async function() {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultPage());
   } catch (err) {
     console.error(err);
   }
